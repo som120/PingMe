e99b242beee05673b8cf859c977b3d9a3218ae62
Somnath Paul <104649891+som120@users.noreply.github.com>
2025-09-26 19:05:10 +0530

    Login screen custom text

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pingme/config/theme/app_theme.dart';
 import 'package:pingme/presentation/screens/auth/login_screen.dart';
 
 void main() {
@@ -13,9 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'PingMe',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
+      theme: AppTheme.lightTheme,
       home: LoginScreen(),
     );
   }
