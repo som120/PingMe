e0d3b9303aa7706255e4a411d35c1b875b92f71a
Somnath Paul <104649891+som120@users.noreply.github.com>
2025-10-11 00:35:40 +0530

    Save work before switching branches

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:pingme/firebase_options.dart';
 import 'package:pingme/presentation/screens/auth/login_screen.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
